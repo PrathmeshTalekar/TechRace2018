f7c92751848dbf12b231e258815b4fa4556aaf7a
PrathmeshTalekar <42110550+PrathmeshTalekar@users.noreply.github.com>
2018-08-22 17:39:27 +0530

    cards layout, error exists

--- app/src/main/java/com/techrace/spit/techrace2018/PowerCardsFragment.java
+++ app/src/main/java/com/techrace/spit/techrace2018/PowerCardsFragment.java
@@ -12,6 +12,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -28,7 +30,7 @@ import static com.techrace.spit.techrace2018.MainActivity.points;
 public class PowerCardsFragment extends Fragment {
     View myView;
     static int twoORfour = 0;
-    Button plusTwo, plusFour, unlockClue;
+    LinearLayout plusTwo, plusFour, unlockClue;
     public PowerCardsFragment() {
         // Required empty public constructor
     }
